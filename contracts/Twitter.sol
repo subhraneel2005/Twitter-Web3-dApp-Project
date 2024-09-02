4b5ad02cffcc23c032703b552df47b0cf807029e
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-03 04:34:23 +0530

    completed learning Structs in solidity and made a simple Twwet struct and updated the post and get functions for tweets. I will get a job, i challange myself :)

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -7,20 +7,38 @@ pragma solidity ^0.8.26;
 // 3️⃣ Add function to create a tweet and save it in mapping
 // 4️⃣ Create a function to get Tweet 
 // 5️⃣ Add array of tweets 
+// 1️⃣ Define a Tweet Struct with author, content, timestamp, likes
+// 2️⃣ Add the struct to array
+// 3️⃣ Test Tweets
 
 contract Twitter{
 
-    mapping(address =>  string[]) public tweets;
-
-    function addTweet(string memory _tweet) public{
-        tweets[msg.sender].push(_tweet);
+    struct Tweet{
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
     }
 
-    function getAllTweets(address _owner) public view returns(string[] memory){
+    mapping(address =>  Tweet[]) public tweets;
+
+    function addTweet(string memory _tweet) public{
+        
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+
+        tweets[msg.sender].push(newTweet);
+    }
+
+    function getAllTweets(address _owner) public view returns(Tweet[] memory){
         return tweets[_owner];
     }
 
-    function getSingleTweetById(address _owner, uint _id) public view returns(string memory){
+    function getSingleTweetById(address _owner, uint _id) public view returns(Tweet memory){
         return tweets[_owner][_id];
     }
 }
