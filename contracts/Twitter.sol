cf99d24ab37d8e7d5ff0f8d374cf2ce69a37dd1c
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-03 04:02:19 +0530

    started the twiiter smart contract code, built a very basic addTweet and getTweet function, learned about mappings, addresses, returns and view

---
+++ contracts/Twitter.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+
+// 1️⃣ Create a Twitter Contract
+// 2️⃣ Create a mapping between user and tweet 
+// 3️⃣ Add function to create a tweet and save it in mapping
+// 4️⃣ Create a function to get Tweet 
+// 5️⃣ Add array of tweets 
+
+contract Twitter{
+
+    mapping(address =>  string) public tweets;
+
+    function addTweet(string memory _tweet) public{
+        tweets[msg.sender] = _tweet;
+    }
+
+    function getTweet(address _owner) public view returns(string memory){
+        return tweets[_owner];
+    }
+}
